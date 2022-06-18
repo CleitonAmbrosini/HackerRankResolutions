8b2535fd62dc982300b9f75de8b1a825e6ef0b6e
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2022-06-18 16:26:00 -0300

    solution for the Mini-Max Sum problem

---
+++ Mini-Max Sum/README.md
@@ -0,0 +1,2 @@
+## Mini-Max Sum
+Link to the problem https://www.hackerrank.com/challenges/mini-max-sum/problem
---
+++ Mini-Max Sum/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    const numbers = arr.sort((a, b) => a - b);
+    const minNumbers = arr.slice(0, 4).reduce((acc, curr) => acc += curr);
+    const maxNumbers = arr.slice(1, 5).reduce((acc, curr) => acc += curr);
+    console.log(minNumbers, maxNumbers);
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
